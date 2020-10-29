50035825dea8bb7932f1d95575013befe5dff0ed
Daniel Rodríguez <sadasant@users.noreply.github.com>
2020-10-29 18:19:42 -0400

    bash update

--- bash.sh
+++ bash.sh
@@ -1,9 +1,20 @@
-echo "IDENTITY_ENDPOINT $IDENTITY_ENDPOINT"
-echo "IDENTITY_HEADER $IDENTITY_HEADER"
+apt-get update
+apt-get install netcat
 
-curl --insecure $IDENTITY_ENDPOINT'?api-version=2019-08-01&resource=https://vault.azure.net/' -H "Secret: $IDENTITY_HEADER"
+message=""
 
-echo "MSI_ENDPOINT $MSI_ENDPOINT"
-echo "MSI_SECRET $MSI_SECRET"
+message="IDENTITY_ENDPOINT $IDENTITY_ENDPOINT\n$message"
+message="IDENTITY_HEADER $IDENTITY_HEADER\n$message"
 
-curl --insecure $MSI_ENDPOINT'?api-version=2017-09-01&resource=https://vault.azure.net/' -H "Secret: $MSI_SECRET"
+response=$(curl --insecure $IDENTITY_ENDPOINT'?api-version=2019-08-01&resource=https://vault.azure.net/' -H "Secret: $IDENTITY_HEADER")
+
+message="response $response\n$message"
+
+message="MSI_ENDPOINT $MSI_ENDPOINT"
+message="MSI_SECRET $MSI_SECRET"
+
+response2=$(curl --insecure $MSI_ENDPOINT'?api-version=2017-09-01&resource=https://vault.azure.net/' -H "Secret: $MSI_SECRET")
+
+message="response2 $response2\n$message"
+
+while true; do echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n$(message)" | nc -l -p 8080; done
